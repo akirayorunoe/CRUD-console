8a9e3d81ee7c982951a83bd866696a9dd14a4d9c
Hoang Pham Huy <intern.phhoang1@tma.com.vn>
2020-08-17 14:38:32 +0700

    CRUD C#

---
+++ internCSharp/internCSharp/Members.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace internCSharp
+{
+    class Members
+    {
+        private int memberId;
+        private string email;
+        private string username;
+        private string firstname;
+        private string lastname;
+        private string birthdate;
+        private string gender;
+        private int weight;
+        public Members()
+        {
+        }
+        public int MemberId  // property
+        {
+            get { return memberId; }   // get method
+            set { memberId = value; }  // set method
+        }
+        public string Email  // property
+        {
+            get { return email; }   // get method
+            set { email = value; }  // set method
+        }
+
+        public string UserName  // property
+        {
+            get { return username; }   // get method
+            set { username = value; }  // set method
+        }
+        public string FirstName  // property
+        {
+            get { return firstname; }   // get method
+            set { firstname = value; }  // set method
+        }
+        public string LastName  // property
+        {
+            get { return lastname; }   // get method
+            set { lastname = value; }  // set method
+        }
+        public string BirthDate  // property
+        {
+            get { return birthdate; }   // get method
+            set { birthdate = value; }  // set method
+        }
+        public string Gender  // property
+        {
+            get { return gender; }   // get method
+            set { gender = value; }  // set method
+        }
+        public int Weight  // property
+        {
+            get { return weight; }   // get method
+            set { weight = value; }  // set method
+        }
+        public void show()
+        {
+            Console.WriteLine("================================");
+            Console.WriteLine("MemberId:" + this.MemberId);
+            Console.WriteLine("Email:" + this.Email.ToString());
+            Console.WriteLine("UserName:" + this.UserName.ToString());
+            Console.WriteLine("FirstName:" + this.FirstName.ToString());
+            Console.WriteLine("LastName:" + this.LastName.ToString());
+            Console.WriteLine("Gender:" + this.Gender.ToString());
+            Console.WriteLine("Weight:" + this.Weight);
+            Console.WriteLine("================================");
+        }
+    }
+}
---
+++ internCSharp/internCSharp/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+namespace internCSharp
+{
+    class Program
+    {
+        private static List<Members> list;
+        public static List<Members> readFromFile()
+        {
+            string json = File.ReadAllText("../../../data.json");
+            var l = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Members>>(json);
+            return l;
+        }
+        public static void showList(List<Members> l)
+        {
+           foreach(var i in l)
+            {
+                Console.WriteLine("================================");
+                Console.WriteLine("MemberId:"+ i.MemberId);
+                Console.WriteLine("Email:"+ i.Email.ToString());
+                Console.WriteLine("UserName:"+i.UserName.ToString());
+                Console.WriteLine("FirstName:"+ i.FirstName.ToString());
+                Console.WriteLine("LastName:"+ i.LastName.ToString());
+                Console.WriteLine("Gender:"+ i.Gender.ToString());
+                Console.WriteLine("Weight:"+ i.Weight);
+                Console.WriteLine("================================");
+            }
+        }
+
+        public static Members createMember()
+        {
+            Members m=new Members();
+            Console.WriteLine("MemberId:");
+            m.MemberId = int.Parse(Console.ReadLine());
+            Console.WriteLine("Email:");
+            m.Email =Console.ReadLine();
+            Console.WriteLine("UserName:");
+            m.UserName = Console.ReadLine();
+            Console.WriteLine("FirstName:");
+            m.FirstName = Console.ReadLine();
+            Console.WriteLine("LastName:");
+            m.LastName = Console.ReadLine();
+            Console.WriteLine("Gender:");
+            m.Gender = Console.ReadLine();
+            Console.WriteLine("Weight:");
+            m.Weight = int.Parse(Console.ReadLine());
+            return m;
+        }
+
+        public static void updateMember(int i)
+        {
+            Members m = new Members();
+            Console.WriteLine("MemberId:");
+            m.MemberId = int.Parse(Console.ReadLine());
+            Console.WriteLine("Email:");
+            m.Email = Console.ReadLine();
+            Console.WriteLine("UserName:");
+            m.UserName = Console.ReadLine();
+            Console.WriteLine("FirstName:");
+            m.FirstName = Console.ReadLine();
+            Console.WriteLine("LastName:");
+            m.LastName = Console.ReadLine();
+            Console.WriteLine("Gender:");
+            m.Gender = Console.ReadLine();
+            Console.WriteLine("Weight:");
+            m.Weight = int.Parse(Console.ReadLine());
+            list.Insert(i, m);
+            list.RemoveAt(i + 1);
+        }
+
+        public static void deleteMember(int i)
+        {
+            list.RemoveAt(i);
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                bool flag = true;
+                list = readFromFile();
+                while (flag)
+                {
+                    Console.WriteLine("Enter number");
+                    Console.WriteLine("1.Show all");
+                    Console.WriteLine("2.Show specific member");
+                    Console.WriteLine("3.Create member");
+                    Console.WriteLine("4.Update member");
+                    Console.WriteLine("5.Delete member");
+                    Console.WriteLine("Other. Exit");
+                    int key = int.Parse(Console.ReadLine());
+                    switch (key)
+                    {
+                        case 1:
+                            showList(list);
+                            break;
+                        case 2:
+                            Console.WriteLine("Choose index of member from 0 to " + (list.Count - 1));
+                            int index = int.Parse(Console.ReadLine());
+                            list[index].show();
+                            break;
+                        case 3:
+                            Members m = createMember();
+                            list.Add(m);
+                            break;
+                        case 4:
+                            Console.WriteLine("Choose index of member from 0 to " + (list.Count - 1) + " to update");
+                            index = int.Parse(Console.ReadLine());
+                            list[index].show();
+                            Console.WriteLine("-----Update to-----");
+                            updateMember(index);
+                            break;
+                        case 5:
+                            Console.WriteLine("Choose index of member from 0 to " + (list.Count - 1) + " to update");
+                            index = int.Parse(Console.ReadLine());
+                            list[index].show();
+                            Console.WriteLine("=====Delete=====");
+                            deleteMember(index);
+                            break;
+                        default:
+                            flag = false;
+                            break;
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
